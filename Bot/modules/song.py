49406a211008fcdd27311ef5c5cc05da47ea05ae
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-04-08 14:23:05 +0530

    Update song.py

--- Bot/modules/song.py
+++ Bot/modules/song.py
@@ -45,7 +45,7 @@ async def song(client, message):
         return ""
     rename = os.rename(download, f"{str(user_id)}.mp3")
     await app.send_chat_action(message.chat.id, "upload_audio")
-    await bot.delete_messages(
+    await app.delete_messages(
         chat_id=message.chat.id,
         message_ids=message.reply_to_message.message_id,
         revoke=True
